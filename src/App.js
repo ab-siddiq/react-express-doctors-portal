919484958ff7866d4d6f410444e4a87f31f47b37
Abu Bakkar Siddiq <abu.bakkar@sebpo.com>
2022-05-18 02:05:44 +0600

    add nav item and components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Appointmnet from './Pages/Appointmnet/Appointmnet';
+import ContactUs from './Pages/ContactUs/ContactUs';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Reviews from './Pages/Reviews/Reviews';
 import Footer from './Shared/Footer/Footer';
 import NavBar from './Shared/NavBar/NavBar';
 
@@ -12,6 +17,11 @@ function App() {
       <NavBar></NavBar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/appointment' element={<Appointmnet></Appointmnet>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/contactUs' element={<ContactUs></ContactUs>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
         {/* <Route path='/' element={}></Route> */}
       </Routes>
       <Footer></Footer>
---
+++ src/Pages/Appointmnet/Appointmnet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Appointmnet = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Appointmnet;
