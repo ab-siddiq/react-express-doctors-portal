0f19e440c9dfb84549be6f08d9feb6ac84d3ee3c
Abu Bakkar Siddiq <abu.bakkar@sebpo.com>
2022-05-18 01:32:52 +0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
+import Footer from './Shared/Footer/Footer';
+import NavBar from './Shared/NavBar/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <button class="btn btn-primary">Button</button>
+    <div>
+
+      <NavBar></NavBar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        {/* <Route path='/' element={}></Route> */}
+      </Routes>
+      <Footer></Footer>
+
     </div>
   );
 }
