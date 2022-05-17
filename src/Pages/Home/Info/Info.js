3b86e2aecfb265f4e192350dcc391f87276009e5
Abu Bakkar Siddiq <abu.bakkar@sebpo.com>
2022-05-18 04:41:53 +0600

    complete card section

--- src/Pages/Home/Info/Info.js
+++ src/Pages/Home/Info/Info.js
@@ -1,10 +1,37 @@
 import React from "react";
 import InfoCard from "./InfoCard";
 import clock from "../../assets/icons/clock.svg";
+import marker from "../../assets/icons/marker.svg";
+import phone from "../../assets/icons/phone.svg";
 const Info = () => {
+  const cardInfos = [
+    {
+      id: 1,
+      title: "Opening Hours",
+      description: "Lorem Ipsum is simply dummy text of the pri",
+      img: clock,
+      color: "bg-gradient-to-r from-primary to-secondary",
+    },
+    {
+      id: 2,
+      title: "Visit our location",
+      description: "Brooklyn, NY 10036, United States",
+      img: marker,
+      color: "bg-accent",
+    },
+    {
+      id: 3,
+      title: "Contact us now",
+      description: "+000 123 456789",
+      img: phone,
+      color: "bg-gradient-to-r from-primary to-secondary",
+    },
+  ];
   return (
-    <div className="">
-      <InfoCard img={clock}></InfoCard>
+    <div class="grid grid-cols-3 justify-items-center gap-10 mx-auto px-12 text-base-100 my-10">
+      {cardInfos.map((cardInfo) => (
+        <InfoCard key={cardInfo.id} info={cardInfo}></InfoCard>
+      ))}
     </div>
   );
 };
--- src/Pages/Home/Info/InfoCard.js
+++ src/Pages/Home/Info/InfoCard.js
@@ -1,34 +1,17 @@
 import React from "react";
 
-const InfoCard = ({ img }) => {
+const InfoCard = ({ info }) => {
+  const { title, description, img, color } = info;
+  console.log(info);
+  console.log("d");
   return (
-    <div class="grid grid-cols-3 justify-items-center gap-4 mx-auto">
-      <div class="card lg:card-side bg-primary shadow-xl px-5">
-        <figure>
-          <img src={img} alt="Album" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">Opening Hours</h2>
-          <p>Lorem Ipsum is simply dummy text of the pri</p>
-        </div>
-      </div>
-      <div class="card lg:card-side bg-primary shadow-xl px-5">
-        <figure>
-          <img src={img} alt="Album" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">Opening Hours</h2>
-          <p>Lorem Ipsum is simply dummy text of the pri</p>
-        </div>
-      </div>
-      <div class="card lg:card-side bg-primary shadow-xl px-5">
-        <figure>
-          <img src={img} alt="Album" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">Opening Hours</h2>
-          <p>Lorem Ipsum is simply dummy text of the pri</p>
-        </div>
+    <div class={`card lg:card-side bg-primary shadow-xl px-5 w-full ${color}`}>
+      <figure>
+        <img src={img} alt="Album" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">{title}</h2>
+        <p>{description}</p>
       </div>
     </div>
   );
