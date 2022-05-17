b4ffe131245d4b849655cc56f250df854be9375c
Abu Bakkar Siddiq <abu.bakkar@sebpo.com>
2022-05-18 05:06:09 +0600

    complete services section

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,46 @@
+import React from "react";
+import fluoride from "../../assets/images/fluoride.png";
+import cavity from "../../assets/images/cavity.png";
+import whitening from "../../assets/images/whitening.png";
+import Service from "./Service";
+
+const Services = () => {
+  const services = [
+    {
+      _id: 1,
+      title: "Fluoride Treatment",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      img: fluoride,
+    },
+    {
+      _id: 2,
+      title: "Cavity Filling",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      img: cavity,
+    },
+    {
+      _id: 3,
+      title: "Teeth Whitening",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      img: whitening,
+    },
+  ];
+  return (
+    <div className="mt-28">
+      <div className="text-center mb-20">
+        <h3 className="text-primary">OUR SERVICES</h3>
+        <h2 className="text-3xl">Services We Provide</h2>
+      </div>
+      <div className="grid grid-cols-3 gap-28 ">
+        {services.map((service) => (
+          <Service key={service._id} service={service}></Service>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
