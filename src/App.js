a3b55b9dfcb3a02a883629c8be40f859f42c472b
Abu Bakkar Siddiq <abu.bakkar@sebpo.com>
2022-05-19 04:22:40 +0600

    add appoint calender

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import About from './Pages/About/About';
-import Appointmnet from './Pages/Appointmnet/Appointmnet';
-import ContactUs from './Pages/ContactUs/ContactUs';
-import Home from './Pages/Home/Home';
-import Login from './Pages/Login/Login';
-import Reviews from './Pages/Reviews/Reviews';
-import Footer from './Shared/Footer/Footer';
-import NavBar from './Shared/NavBar/NavBar';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Pages/About/About";
+import Appointmnet from "./Pages/Appointmnet/Appointmnet";
+import ContactUs from "./Pages/ContactUs/ContactUs";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Reviews from "./Pages/Reviews/Reviews";
+import Footer from "./Shared/Footer/Footer";
+import NavBar from "./Shared/NavBar/NavBar";
 
 function App() {
   return (
     <div>
-
       <NavBar></NavBar>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
-        <Route path='/appointment' element={<Appointmnet></Appointmnet>}></Route>
-        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
-        <Route path='/contactUs' element={<ContactUs></ContactUs>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route
+          path="/appointment"
+          element={<Appointmnet></Appointmnet>}
+        ></Route>
+        <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route path="/contactUs" element={<ContactUs></ContactUs>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
         {/* <Route path='/' element={}></Route> */}
       </Routes>
       <Footer></Footer>
-
     </div>
   );
 }
--- src/Pages/Appointmnet/Appointmnet.js
+++ src/Pages/Appointmnet/Appointmnet.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import { format } from "date-fns";
+import React, { useState } from "react";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import chair from "../../assets/images/chair.png";
 
-const Appointmnet = () => {
-    return (
-        <div>
-            
+const Appointment = () => {
+  const [date, setDate] = useState(new Date());
+  return (
+    <section>
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <DayPicker mode="single" selected={date} onSelect={setDate} />
+            <h1 class=" font-bold">Your Appoinment On: {format(date, "PP")}</h1>
+          </div>
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
-export default Appointmnet;
+export default Appointment;
