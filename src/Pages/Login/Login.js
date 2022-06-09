384be83b979144a26d4f7463f3cf1b2c1721777a
Abu Bakkar Siddiq <abu.bakkar@sebpo.com>
2022-06-10 00:21:23 +0600

    add google sign in

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,27 +1,30 @@
 import React, { useState } from "react";
-
+import auth from "../../firebase.init";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 const Login = () => {
-  const [user, setUser] = useState({});
+  // const [user, setUser] = useState({});
+  const [signInWithGoogle, user, loading, error] =
+    useSignInWithGoogle(auth);
+   
+  // const handleLoginSubmit = (e) => {
+  //   e.preventDefault();
+  //   const user = e.target.email.value;
+  //   const password = e.target.password.value;
+  //   const users = { user, password };
 
-  const handleLoginSubmit = (e) => {
-    e.preventDefault();
-    const user = e.target.email.value;
-    const password = e.target.password.value;
-    const users = { user, password };
-
-    fetch("http://localhost:5000/user", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(users),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  };
+  //   fetch("http://localhost:5000/user", {
+  //     method: "POST",
+  //     headers: {
+  //       "content-type": "application/json",
+  //     },
+  //     body: JSON.stringify(users),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
+  // };
   return (
-    <div className="">
-      <form onSubmit={handleLoginSubmit}>
+    <div className="flex justify-center items-center h-screen">
+      {/* <form onSubmit={handleLoginSubmit}>
         <input
           type="email"
           name="email"
@@ -40,7 +43,19 @@ const Login = () => {
           value="login"
           class="input input-bordered w-full max-w-xs"
         />
-      </form>
+      </form> */}
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title">Please Login!</h2>
+          <div class="divider">OR</div>
+          <button
+            class="btn btn-outline btn-secondary"
+            onClick={()=>signInWithGoogle()}
+          >
+            Sign in with Google
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
