db14e2b742cac3f956d8f401c31ffccc225d16af
Abu Bakkar Siddiq <abu.bakkar@sebpo.com>
2022-06-22 20:30:22 +0600

    create user or admin

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { useQuery } from "react-query";
 import Loading from "../../Shared/Loading";
+import User from "./User";
 
 const Users = () => {
-  const { data: users, isLoading } = useQuery("users", () =>
-    fetch("http://localhost:5000/user").then((res) => res.json())
+  const { data: users, isLoading, refetch } = useQuery("users", () =>
+    fetch("http://localhost:5000/user",{
+        method: "GET",
+        headers: {
+            "authorization": `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then((res) => res.json())
   );
-  console.log(users);
+//   console.log(users);
   //   const { data: users, isLoading} = useQuery('users' , () => fetch('http://localhost:5000/user').then(res=>res.send())) ;
   if (isLoading) {
     return <Loading></Loading>;
   }
   return (
-    <div class="overflow-x-auto">
-      <table class="table table-compact w-full">
+    <div className="overflow-x-auto">
+      <table className="table table-compact w-full">
         <thead>
           <tr>
             <th>SL</th>
@@ -25,22 +31,7 @@ const Users = () => {
         </thead>
         <tbody>
           {users.map((user, index) => (
-            <tr>
-              <td>{index + 1}</td>
-              <td>
-                <div class="mask mask-squircle w-12 h-12 flex items-center">
-                  <img
-                    src="/tailwind-css-component-profile-2@56w.png"
-                    alt="Avatar Tailwind CSS Component"
-                  />
-                </div>
-              </td>
-              <td>{user.email}</td>
-              <td>{user._id}</td>
-              <td>
-                d
-              </td>
-            </tr>
+            <User key={user._id} user={user} index={index} refetch={refetch}></User>
           ))}
         </tbody>
       </table>
