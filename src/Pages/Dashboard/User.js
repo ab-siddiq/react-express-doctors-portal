7e82feda2e0e9ef2f0ea6492a24f9d654d13e479
siddiq <siddiq.aiub2gmail.com>
2022-06-23 12:19:39 +0600

    fix design

--- src/Pages/Dashboard/User.js
+++ src/Pages/Dashboard/User.js
@@ -1,55 +1,62 @@
-import React from 'react';
-const User = ({user,refetch,index}) => {
-    // console.log(user)
-    const {email,role} = user;
-    const makeAdmin = () =>{
-        fetch(`http://localhost:5000/user/admin/${user.email}`,{
-            method: 'PUT',
-            headers: {
-                authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-            }
-        })
-        .then(res=>res.json())
-        .then(data=>{
-            refetch();
-        })
-    }
-    const makeUser = () =>{
-        fetch(`http://localhost:5000/user/user/${user.email}`,{
-            method: 'PUT',
-            headers: {
-                authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-            }
-        })
-        .then(res=>res.json())
-        .then(data=>{
-            refetch();
-        })
-    }
-    return (
-      <tr>
-        <td>{index + 1}</td>
-        <td>
-          <div className="mask mask-squircle w-12 h-12 flex items-center">
-            <img
-              src="/tailwind-css-component-profile-2@56w.png"
-              alt="Avatar Tailwind CSS Component"
-            />
-          </div>
-        </td>
-        <td>{user.email}</td>
-        <td>{user._id}</td>
-        <td>
-          {role !== "admin" || !role ? <button className="btn btn-success mx-1" onClick={makeAdmin}>Make Admin</button>:(
-            <button className="btn btn-success mx-1" onClick={makeUser}>
-              Make User
-            </button>
-          )}
-          <button className="btn btn-info mx-1">Edit</button>
-          <button className="btn btn-warning mx-1">Delete</button>
-        </td>
-      </tr>
-    );
+import React from "react";
+const User = ({ user, refetch, index }) => {
+  // console.log(user)
+  const { email, role } = user;
+  const makeAdmin = () => {
+    fetch(`http://localhost:5000/user/admin/${user.email}`, {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        refetch();
+      });
+  };
+  const makeUser = () => {
+    fetch(`http://localhost:5000/user/user/${user.email}`, {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        refetch();
+      });
+  };
+  return (
+    <tr>
+      <td>{index + 1}</td>
+      <td>
+        <div className="mask mask-squircle w-12 h-12 flex items-center">
+          <img
+            src="/tailwind-css-component-profile-2@56w.png"
+            alt="Avatar Tailwind CSS Component"
+          />
+        </div>
+      </td>
+      <td>{user.email}</td>
+      <td>{user._id}</td>
+      <td>
+        
+        {role !== "admin" || !role ? (
+          <button className="btn w-[120px] btn-success mx-1" onClick={makeAdmin}>
+            Make Admin
+          </button>
+        ) : (
+          <button className="btn w-[120px] btn-success mx-1" onClick={makeUser}>
+            Make User
+          </button>
+        )}
+
+        <button className="btn w-[120px] btn-info mx-1">Edit</button>
+
+        <button className="btn w-[120px] btn-warning mx-1">Delete</button>
+      </td>
+    </tr>
+  );
 };
 
 export default User;
